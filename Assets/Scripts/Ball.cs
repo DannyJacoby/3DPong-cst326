5f27564a13bb9a3bc5cd2a5873cd8cca8acdc5ed
DannyJacoby <60196483+DannyJacoby@users.noreply.github.com>
2021-02-04 19:55:42 -0800

    All working, sound plays on all the hits
    Fantastico game design if I do say so myself

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -10,6 +10,11 @@ public class Ball : MonoBehaviour
     public float speed = 5f;
     public Text txt;
 
+    public AudioClip goalSound;
+    public AudioClip impactSound;
+
+    private AudioSource _audioSource;
+
     private int scoreP1 = 0;
     private int scoreP2 = 0;
 
@@ -18,6 +23,11 @@ public class Ball : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // goalSound = GetComponent<AudioClip>();
+        // impactSound = GetComponent<AudioClip>();
+
+        _audioSource = GetComponent<AudioSource>();
+        
         float rx = Random.Range(0, 2) == 0 ? -1 : 1;
         float ry = Random.Range(0, 2) == 0 ? -1 : 1;
 
@@ -31,24 +41,29 @@ public class Ball : MonoBehaviour
     {
         if (scoreP1 == 11)
         {
+            PlaySound(goalSound);
             var str = "P1 WINS";
             txt.text = str;
+            Debug.Log(str);
             ResetGame();
 
         } 
         if (scoreP2 == 11)
         {
+            PlaySound(goalSound);
             var str = "P2 WINS";
             txt.text = str;
+            Debug.Log(str);
             ResetGame();
         }
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        
+        // within the player hit, check where it is on the bumper and increase/decrease speed accordingly
         if (other.gameObject.CompareTag("GoalRight"))
         {
+            PlaySound(goalSound);
             scoreP1++;
             Debug.Log("Hit Right Goal P1 Points: " + scoreP1);
             var str = scoreP1 + " -- " + scoreP2;
@@ -57,6 +72,7 @@ public class Ball : MonoBehaviour
         } 
         if (other.gameObject.CompareTag("GoalLeft"))
         {
+            PlaySound(goalSound);
             scoreP2++;
             Debug.Log("Hit Left Goal P2 Points: " + scoreP2);
             var str = scoreP1 + " -- " + scoreP2;
@@ -66,9 +82,15 @@ public class Ball : MonoBehaviour
         
         if (other.gameObject.CompareTag("Player"))
         {
+            PlaySound(impactSound);
             var NewSpeed = speed + 2f;
             Debug.Log("Hit Bumper Increase Speed to: " + NewSpeed);
             speed = NewSpeed;
+        }
+
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            PlaySound(impactSound);
         }
 
         //Debug.Log("Hit a wall");
@@ -88,6 +110,12 @@ public class Ball : MonoBehaviour
         scoreP2 = 0;
         var str = scoreP1 + " -- " + scoreP2;
         txt.text = str;
+    }
+
+    void PlaySound(AudioClip soundClip)
+    {
+        _audioSource.clip = soundClip;
+        _audioSource.Play();
     }
     
 }
