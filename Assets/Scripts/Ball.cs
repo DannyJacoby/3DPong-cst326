2103d417bb8bfbd6c3b8fb9c20e94de10a718f76
DannyJacoby <60196483+DannyJacoby@users.noreply.github.com>
2021-02-10 11:29:26 -0800

    Recent push for Mark II

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,14 +8,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
+[RequireComponent(typeof(AudioSource))]
 public class Ball : MonoBehaviour
 {
-    public float speed = 5f;
-    public Text txt;
+    public float speed = 10f;
+    public TextMeshProUGUI txt;
+    public TextMeshProUGUI p1ScoreTxt;
+    public TextMeshProUGUI p2ScoreTxt;
     public float dynamicDifficulty = 1f;
 
     public AudioClip goalSound;
@@ -27,15 +31,17 @@ public class Ball : MonoBehaviour
     private int _scoreP2 = 0;
 
     private Vector3 _originalPosition;
+
+    // private float r = 0.2f, g = 0.3f, b = 0.7f, a = 0.6f;
+
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
     
     // Start is called before the first frame update
     void Start()
     {
-        // goalSound = GetComponent<AudioClip>();
-        // impactSound = GetComponent<AudioClip>();
-
-        _audioSource = GetComponent<AudioSource>();
-        
         float rx = Random.Range(0, 2) == 0 ? -1 : 1;
         float ry = Random.Range(0, 2) == 0 ? -1 : 1;
 
@@ -47,24 +53,33 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_scoreP1 == 11)
-        {
-            PlaySound(goalSound);
-            var str = "Game Over, P1 WINS";
-            txt.text = str;
-            Debug.Log(str);
-            ResetGame();
-            dynamicDifficulty += 10f;
-        } 
-        if (_scoreP2 == 11)
-        {
-            PlaySound(goalSound);
-            var str = "Game Over, P2 WINS";
-            txt.text = str;
-            Debug.Log(str);
-            ResetGame();
-            dynamicDifficulty += 10f;
-        }
+        
+        if (_scoreP1 < _scoreP2) { p1ScoreTxt.color = Color.red; }
+        else if (_scoreP1 == _scoreP2) { p1ScoreTxt.color = Color.white; }
+        else { p1ScoreTxt.color = Color.green; }
+        if (_scoreP2 < _scoreP1) { p2ScoreTxt.color = Color.red; }          
+        else if (_scoreP1 == _scoreP2) { p2ScoreTxt.color = Color.white; }
+        else { p2ScoreTxt.color = Color.green; }
+        
+        
+        // if (_scoreP1 == 11)
+        // {
+        //     PlaySound(goalSound);
+        //     var str = "Game Over, P1 WINS";
+        //     txt.text = str;
+        //     Debug.Log(str);
+        //     ResetGame();
+        //     dynamicDifficulty += 10f;
+        // } 
+        // if (_scoreP2 == 11)
+        // {
+        //     PlaySound(goalSound);
+        //     var str = "Game Over, P2 WINS";
+        //     txt.text = str;
+        //     Debug.Log(str);
+        //     ResetGame();
+        //     dynamicDifficulty += 10f;
+        // }
     }
 
     private void OnCollisionEnter(Collision other)
@@ -72,20 +87,52 @@ public class Ball : MonoBehaviour
         // within the player hit, check where it is on the bumper and increase/decrease speed accordingly
         if (other.gameObject.CompareTag("GoalRight"))
         {
+            var str = "";
+            if (_scoreP1 == 11)
+            {
+                PlaySound(goalSound);
+                str = "Game Over, P1 WINS";
+                txt.text = str;
+                Debug.Log(str);
+                ResetGame();
+                dynamicDifficulty += 10f;
+                return;
+            } 
             PlaySound(goalSound);
             _scoreP1++;
             Debug.Log("Hit Right Goal P1 Points: " + _scoreP1);
-            var str = _scoreP1 + " -- " + _scoreP2;
-            txt.text = str;
+            str = _scoreP1.ToString();
+            p1ScoreTxt.text = str;
+            
+            // if (_scoreP1 < _scoreP2) { p1ScoreTxt.color = Color.red; }
+            // else if (_scoreP1 == _scoreP2) { p1ScoreTxt.color = Color.white; }
+            // else { p1ScoreTxt.color = Color.green; }
+            
             ResetBallPosition(other);
         } 
         if (other.gameObject.CompareTag("GoalLeft"))
         {
+            var str = "";
+            if (_scoreP2 == 11)
+            {
+                PlaySound(goalSound);
+                str = "Game Over, P2 WINS";
+                txt.text = str;
+                Debug.Log(str);
+                ResetGame();
+                dynamicDifficulty += 10f;
+                return;
+            }
             PlaySound(goalSound);
             _scoreP2++;
             Debug.Log("Hit Left Goal P2 Points: " + _scoreP2);
-            var str = _scoreP1 + " -- " + _scoreP2;
-            txt.text = str;
+            str = _scoreP2.ToString();
+            p2ScoreTxt.text = str;
+            
+            // if (_scoreP2 < _scoreP1) { p2ScoreTxt.color = Color.red; }          
+            // else if (_scoreP1 == _scoreP2) { p2ScoreTxt.color = Color.white; }
+            // else { p2ScoreTxt.color = Color.green; }
+            
             ResetBallPosition(other);
         }
         
